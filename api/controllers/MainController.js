3a82869480422c9441e6fe684d742e5b55c6521c
timestep <jbosshellokitty@gmail.com>
2013-08-28 17:40:59 -0400

    added sign up and login logic for controller

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -3,10 +3,51 @@ var MainController = {
 		res.view();	
 	},
 	signup:function(req,res){
-	
+		var username = req.param("username");
+		var password = req.param("password");
+		
+		Users.findByUsername(username).done(function(err,usr){
+			if(err){
+				res.send(500, { error: "DB Error" });
+			} else if (usr) {
+				res.send(400, { error: "Username already Taken" });
+			} else {
+				var hasher = require("password-hash");
+				password = hasher.generate(password);
+
+				User.create({username: username, password: password})
+				    .done(function(error,user){
+					    if(error){
+						    res.send(500, {error: "DB Error"});
+					    } else {
+						    req.session.user = user;
+						    req.send(user);
+					    }
+				});
+			}
+		}	
 	},
 	login:function(req,res){
-	
+		var username = req.param("username");
+		var password = req.param("password");
+
+		Users.findByUsername(username).done(function(err,usr){
+			if(err){
+				res.send(500, { error: "DB Error" });
+			} else {
+				if (usr) {
+					var hasher = require("password-hash");
+					if (hasher.verify(password, usr.password)){
+						req.session.user = usr;
+						res.send(usr);
+					} else {
+						res.send(400, { error: "Wrong Password" });
+					}
+				} else {
+					res.send(404, { error: "User not Found"});
+				}
+			}
+		}	
 	},
 	chat:function(req,res){
 
